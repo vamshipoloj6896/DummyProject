127a6b991cfa485cf23b02ee9d75435fdc68d942
Vamshi <Vamshi@DESKTOP-K8260M9>
2023-06-21 15:15:00 +0530

    eng2 thirdd commit

--- DemoPro/src/main/java/demo/Demo.java
+++ DemoPro/src/main/java/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("login module done");
-		System.out.println("Home page module done"); 
+		System.out.println("Home page module done");  
 
 	}
 
--- DemoPro/src/main/java/demo/Demo2.java
+++ DemoPro/src/main/java/demo/Demo2.java
@@ -3,7 +3,7 @@ package demo;
 public class Demo2 {
 
 	public static void main(String[] args) {
-		System.out.println("profile module done");
+		System.out.println("profile module done"); 
 	}
 
 }
